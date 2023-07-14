d29878159878e562c0aa4294a5ccdaa6ff9680d9
OliviaDemetriou <oliviadem97@gmail.com>
2023-07-14 17:07:06 +0100

    Making left & right buttons work

--- app.js
+++ app.js
@@ -3,118 +3,49 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
 
-// const track = document.querySelector(".carousel");
-// const slides = Array.from(track.children);
-// const nextButton = document.querySelector(".btn--right");
-// const prevButton = document.querySelector(".btn--left");
-// const dotsNav = document.querySelector(".dots");
-// const dots = Array.from(dotsNav.children);
-
-// const buttons = document.querySelectorAll("[data-carousel-button]");
-
 menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
 
-// buttons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-//     const slides = button
-//       .closest("[data-backcarousel]")
-//       .querySelector("[data-slides]");
-
-//     const activeSlide = slides.querySelector("[data-active]");
-//     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-//     if (newIndex < 0) newIndex = slides.children.length - 1;
-//     if (newIndex >= slides.children.length) newIndex = 0;
-
-//     slides.children[newIndex].dataset.active = true;
-//     delete activeSlide.dataset.active;
-//   });
-// });
-
 // Slider
-const slider = function () {
-  const slides = document.querySelectorAll(".carousel");
-  const btnLeft = document.querySelector(".btn--left");
-  const btnRight = document.querySelector(".btn--right");
-  const dotContainer = document.querySelector(".dots");
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".btn--left");
+const btnRight = document.querySelector(".btn--right");
 
-  let curSlide = 0;
-  const maxSlide = slides.length;
+let curSlide = 0;
+const maxSlide = slides.length;
 
-  // Functions
-  const createDots = function () {
-    slides.forEach(function (_, i) {
-      dotContainer.insertAdjacentHTML(
-        "beforeend",
-        `<button class="dot" data-dot--fill="${i}"></button>`
-      );
-    });
-  };
+// const slider = document.querySelector(".slider");
+// slider.style.transform = "scale()";
+// slider.style.overflow = "visible";
 
-  const activateDot = function (slide) {
-    document
-      .querySelectorAll(".dot")
-      .forEach((dot) => dot.classList.remove("dot--fill"));
-
-    document
-      .querySelector(`.dots__dot[data-slide="${slide}"]`)
-      .classList.add("dot--fill");
-  };
-
-  const goToSlide = function (slide) {
-    slides.forEach(
-      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-    );
-  };
-
-  // Next slide
-  const nextSlide = function () {
-    if (curSlide === maxSlide - 1) {
-      curSlide = 0;
-    } else {
-      curSlide++;
-    }
-
-    goToSlide(curSlide);
-    activateDot(curSlide);
-  };
-
-  const prevSlide = function () {
-    if (curSlide === 0) {
-      curSlide = maxSlide - 1;
-    } else {
-      curSlide--;
-    }
-    goToSlide(curSlide);
-    activateDot(curSlide);
-  };
-
-  const init = function () {
-    goToSlide(0);
-    createDots();
-
-    activateDot(0);
-  };
-  init();
-
-  // Event handlers
-  btnRight.addEventListener("click", nextSlide);
-  btnLeft.addEventListener("click", prevSlide);
-
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") prevSlide();
-    e.key === "ArrowRight" && nextSlide();
-  });
-
-  dotContainer.addEventListener("click", function (e) {
-    if (e.target.classList.contains("dots__dot")) {
-      const { slide } = e.target.dataset;
-      goToSlide(slide);
-      activateDot(slide);
-    }
-  });
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%`)
+  );
 };
-slider();
+goToSlide(0);
+
+// Next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
